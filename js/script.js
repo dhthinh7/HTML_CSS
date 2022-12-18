cc238116844c494cdf2fc6f2ea70d580c5600eb5
dhthinh7 <dhthinh7@gmail.com>
2022-12-19 01:02:26 +0700

    update

--- js/script.js
+++ js/script.js
@@ -5,20 +5,20 @@ const items = document.getElementById('main__items')
 
 const renderItems = function (listItems) {
   return listItems.reduce((result, item) => {
-    return result += `
-      <div class="item item--0">
-        <div class="item__day">${item.day}</div>
-        <a href=${item.link} target="_blank">
-          <div class="item__overlay">
-            <h3>${item.tittle}</h3>
-            <p>${item.description}</p>
-          </div>
-        </a>
-        <div class="content__main">
-          <div class="item--background" style="background-image: url(${item.img});"></div>
+    return item.isShow ? result += `
+    <div class="item item--0">
+      <div class="item__day">${item.day}</div>
+      <a href=${item.link} target="_blank">
+        <div class="item__overlay">
+          <h3>${item.tittle}</h3>
+          <p>${item.description}</p>
         </div>
+      </a>
+      <div class="content__main">
+        <div class="item--background" style="background-image: url(${item.img});"></div>
       </div>
-    `
+    </div>
+  ` : result
   }, '')
 }
 
